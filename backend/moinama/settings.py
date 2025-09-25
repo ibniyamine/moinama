9902a980bae9c30bcf193b01e0336291ba3141df
laspi@ <laspi@gmail.com>
2025-09-25 17:32:27 +0000

    dashbnord improved

--- backend/moinama/settings.py
+++ backend/moinama/settings.py
@@ -185,3 +185,7 @@ AUTH_USER_MODEL = 'accounts.User'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authentication redirects
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/dashboard/'
